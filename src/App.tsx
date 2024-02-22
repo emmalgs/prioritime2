32f1c9bb7f96c023985111a1e10751956bad1a94
Emma Gerigscott <elgerig@gmail.com>
2024-02-22 12:00:54 -0800

    Refactor App component to use DayInput and DayOutput components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Form from "./components/form";
+import DayInput from "./components/dayInput";
+import DayOutput from "./components/dayOutput";
+import {chatGpt} from "./services/chatApi";
+import { useState } from "react";
 
 const App = () => {
+  const [output, setOutput] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const allInputs = e.target.todo;
+    const values = [];
+    for (let i = 0; i < allInputs.length; i++) {
+      values.push(allInputs[i].value);
+    }
+    values.join(' ,');
+    const response = await chatGpt(values);
+    setOutput(response);
+  }
 
   return (
-    <div>
-      <h1>What do you need to get done today?</h1>
-      <Form />
+    <div className="main">
+      <DayInput handleSubmit={handleSubmit} />
+      <DayOutput response={output} />
     </div>
   );
 };
