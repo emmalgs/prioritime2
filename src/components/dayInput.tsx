a7a4bd96741bb08b84cbe62f81dec5d62c3ccfbf
Emma Gerigscott <elgerig@gmail.com>
2024-02-22 13:49:40 -0800

    Update dayInput component with new form structure

--- src/components/dayInput.tsx
+++ src/components/dayInput.tsx
@@ -3,10 +3,18 @@ import Form from "./form";
 const DayInput = ({ handleSubmit }) => {
   return (
     <div className="dayMain">
-      <h1 className="dayHeader">What do you need to get done today?</h1>
-      <Form handleSubmit={handleSubmit} />
+      <h2 className="dayHeader">What do you need to get done today?</h2>
+      <form className='inputForm' onSubmit={handleSubmit}>
+        <Form category={"work"} />
+        <Form category={"chores"} />
+        <Form category={"needs"} />
+        <Form category={"dreams"} />
+        <p>
+          Plan my day <button type="submit">&#8594;</button>
+        </p>
+      </form>
     </div>
   );
-}
+};
 
 export default DayInput;
