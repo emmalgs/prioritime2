d0d89f7336f0db1aa99dde4909a8e4be584b0b3c
Emma Gerigscott <elgerig@gmail.com>
2024-02-22 15:39:19 -0800

    Add output component and functionality to DayOutput

--- src/components/dayOutput.tsx
+++ src/components/dayOutput.tsx
@@ -1,11 +1,45 @@
+import Output from "./output";
+import { useState } from "react";
+
 const DayOutput = ({ response }) => {
+  const [values, setValues] = useState<any[]>([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const outputs = e.currentTarget.output;
+    const outputValues = Array.from(outputs).map(
+      (output: HTMLInputElement) => output.value
+    );
+    setValues(outputValues);
+  };
+
+  const outputView =
+    values.length > 0 ? (
+      <div>
+        <ul>
+          {values.map((value, index) => {
+            return <li key={index}>{value}</li>;
+          })}
+        </ul>
+        <button onClick={() => setValues([])}>Edit</button>
+      </div>
+    ) : (
+      <form onSubmit={handleSubmit}>
+        {response.map((item, index) => {
+          if (item === "") {
+            return null;
+          } else {
+            return <Output key={index} text={item} />;
+          }
+        })}
+        <button type="submit">Save</button>
+      </form>
+    );
+
   return (
     <div className="dayOutput">
       <h1>Your Day:</h1>
-      {response.map((item, index) => {
-        return <p key={index}>{item}</p>;
-      })
-    }
+      {outputView}
     </div>
   );
 };
---
+++ src/components/output.tsx
@@ -0,0 +1,5 @@
+const Output = ({ text }) => {
+  return <input type="text" value={text} className="dayInput" name="output" />;
+};
+
+export default Output;
