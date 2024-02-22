648513ff41658fd58a09f649aaf23773df1c1475
Emma Gerigscott <elgerig@gmail.com>
2024-02-22 12:01:27 -0800

    Refactor form component to dynamically add and remove inputs

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
 import Input from "./input";
 
-const Form = () => {
-  const [inputCount, setInputCount] = useState(1);
+const Form = ({ handleSubmit }) => {
+  const [inputs, setInputs] = useState([{ key: 0 }]);
 
   const addInput = () => {
-    setInputCount(inputCount + 1);
+    setInputs([...inputs, { key: inputs.length }]);
   };
 
-  const inputs = Array.from({ length: inputCount }).map((_, index) => (
-    <Input key={index} />
-  ));
+  const removeInput = (index) => {
+    const newInputs = inputs.filter((input) => input.key !== index);
+    setInputs(newInputs);
+  };
+
+  const inputView = inputs.map((input, index) => {
+    return (
+      <Input key={input.key} remove={() => removeInput(input.key)} />
+    );
+  });
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const allInputs = e.target.todo;
+  //   const values = [];
+  //   for (let i = 0; i < allInputs.length; i++) {
+  //     values.push(allInputs[i].value);
+  //   }
+  //   values.join(' ,');
+  //   const response = await chatGpt(values);
+  // }
 
   return (
     <div>
-      <button onClick={addInput}>+</button>
-      <form className='inputForm'>
-        {inputs}
-        <button type="submit">Submit</button>
+      <button type="button" className="addInputBtn" onClick={addInput}>+</button>
+      <form className='inputForm' onSubmit={handleSubmit}>
+        {inputView}
+        <p>Plan my day <button type="submit">&#8594;</button></p>
       </form>
     </div>
   );
